2c745f24087a92f45bedb926a30195d7b4e0fd67
MSFirsov <mxfrsv@gmail.com>
2025-03-15 23:28:44 +0300

    added basic bot

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-def main():
-    print("Hello from t-bot!")
+import asyncio
+import logging
+import os
+
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from dotenv import load_dotenv
 
 
-if __name__ == "__main__":
-    main()
+load_dotenv()
+
+token = os.getenv('TOKEN')
+
+bot = Bot(token=token)
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def cmd_start(message: Message):
+    await message.answer('Привет!')
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
